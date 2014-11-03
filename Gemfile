dd7a735f5c8a5e172e5273e5ca3df54959067f34
Rasheed <secr4sh@gmail.com>
2014-11-02 23:48:47 -0500

    Upated files with  test code for google_places gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source "https://rubygems.org"
 gem 'sinatra'
-gem 'geocoder'
+gem 'google_places'
--- main.rb
+++ main.rb
@@ -1,17 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'geocoder'
+require 'google_places'
 
 
 get '/' do
   "What's For Lunch on Sinatra!"
 end
 
-get '/geolocation' do
+get '/geo' do
+  
+  @client = GooglePlaces::Client.new("AIzaSyB3xKb4v0cK805_F1ApSX0Os0KS-XzDoO4")
 
-  city = request.location
-  country = request.location.country_code
+  lat = 151.1957362
+  lon = -33.8670522
 
-  "City: #{city}. Country: #{country}"
+
+  @spots_list = @client.spots(lon, lat, :types => 'restaurant')
+
+  erb :geo
+
 
 end
